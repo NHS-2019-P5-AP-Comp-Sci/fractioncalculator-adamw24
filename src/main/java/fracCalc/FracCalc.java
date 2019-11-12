6bdc8e1ac63833755bd43cf58d9b14b766e72c6d
s-wangad <s-wangad@53S50030263.it.bsd405.org>
2019-11-12 11:51:39 -0800

    Checkpoint 1

--- src/main/java/fracCalc/FracCalc.java
+++ src/main/java/fracCalc/FracCalc.java
@@ -4,29 +4,88 @@
 
 package fracCalc;
 
+import java.util.*;
+
+import org.checkerframework.checker.units.qual.s;
+
 public class FracCalc {
+	public static void main(String[] args) {
+		Scanner userInput = new Scanner(System.in);
+		// TODO: Read the input from the user and call produceAnswer with an equation
+		String input = userInput.nextLine();
+		System.out.println(produceAnswer(input));
+		userInput.close();
+	}
 
-    public static void main(String[] args)
-    {
-        // TODO: Read the input from the user and call produceAnswer with an equation
+	// ** IMPORTANT ** DO NOT DELETE THIS FUNCTION. This function will be used to
+	// test your code
+	// This function takes a String 'input' and produces the result
+	//
+	// input is a fraction string that needs to be evaluated. For your program, this
+	// will be the user input.
+	// e.g. input ==> "1/2 + 3/4"
+	//
+	// The function should return the result of the fraction after it has been
+	// calculated
+	// e.g. return ==> "1_1/4"
 
-    }
+	/*
+	 * public static String produceAnswer(String input) { int totalNumer = 0; int
+	 * totalDenom = 1; int totalWhole = 0; if (input.charAt(0) == ' ') { input =
+	 * input.substring(1); } int firstSpace = input.indexOf(" "); String
+	 * firstOperand = input.substring(0, firstSpace); int wholeF =
+	 * whole(firstOperand); int numerF = numerator(firstOperand); int denomF =
+	 * denominator(firstOperand); char operator = input.charAt(firstSpace + 1);
+	 * String secondOperand = input.substring(firstSpace + 3); int wholeS =
+	 * whole(secondOperand); int numerS = numerator(secondOperand); int denomS =
+	 * denominator(secondOperand); if (operator == '+') { totalWhole = wholeF +
+	 * wholeS; totalNumer = numerF * denomS + numerS * denomF; } else if (operator
+	 * == '-') { totalWhole = wholeF - wholeS; totalNumer = numerF * denomS - numerS
+	 * * denomF; } else if (operator == '*') { totalNumer = (wholeF*denomF + numerF)
+	 * (wholeS*denomS + numerS); totalDenom = denomF * denomS; total = "" +
+	 * totalNumer + "/" + totalDenom; } totalDenom = denomF * denomS; String total =
+	 * "" + totalWhole + "_" + totalNumer + "/" + totalDenom; return total; }
+	 */
 
-    // ** IMPORTANT ** DO NOT DELETE THIS FUNCTION.  This function will be used to test your code
-    // This function takes a String 'input' and produces the result
-    //
-    // input is a fraction string that needs to be evaluated.  For your program, this will be the user input.
-    //      e.g. input ==> "1/2 + 3/4"
-    //
-    // The function should return the result of the fraction after it has been calculated
-    //      e.g. return ==> "1_1/4"
-    public static String produceAnswer(String input)
-    {
-        // TODO: Implement this function to produce the solution to the input
+	public static String produceAnswer(String input) {
+		if (input.charAt(0) == ' ') {
+			input = input.substring(1);
+		}
+		int firstSpace = input.indexOf(" ");
+		String firstOperand = input.substring(0, firstSpace);
+		firstOperand = "whole:" + whole(firstOperand) + "numerator:" + numerator(firstOperand) + "denominator:"
+				+ denominator(firstOperand);
+		String operator = input.substring(firstSpace + 1, firstSpace + 2);
+		String secondOperand = input.substring(firstSpace + 3);
+		secondOperand = "whole:" + whole(secondOperand) + " numerator:" + numerator(secondOperand) + " denominator:"
+				+ denominator(secondOperand);
+		return secondOperand;
+	}
 
-        return "";
-    }
+	public static int whole(String operand) {
+		int whole = 0;
+		if (operand.indexOf("_") != -1) {
+			whole = Integer.parseInt(operand.substring(0, operand.indexOf("_")));
+		} else if (operand.indexOf("/") == -1) {
+			whole = Integer.parseInt(operand);
+		}
+		return whole;
+	}
 
-    // TODO: Fill in the space below with any helper methods that you think you will need
+	public static int numerator(String operand) {
+		int numer = 0;
+		if (operand.indexOf("/") != -1) {
+			numer = Integer.parseInt(operand.substring(operand.indexOf("_") + 1, operand.indexOf("/")));
+		}
+		return numer;
+	}
+
+	public static int denominator(String operand) {
+		int denom = 1;
+		if (operand.indexOf("/") != -1) {
+			denom = Integer.parseInt(operand.substring(operand.indexOf("/") + 1));
+		}
+		return denom;
+	}
 
 }
